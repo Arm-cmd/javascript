221e1502e61fcf93aaadbf194c3d82f3fbf2ab08
Arm-cmd <armanthakur819@gmail.com>
2026-04-15 16:47:25 +0000

    datatype Conversions

---
+++ 01_basics/03_datatypeConversions.js
@@ -0,0 +1,47 @@
+let score = 33
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(typeof (valueInNumber))
+
+// 33 => 33 number converets to number 
+// 33abc => Nan (not a number) but it's type is still a number
+// true =>1 ; false =>0 
+// null => 0 , undefined => Nan 
+
+
+let booleanIsLogged = 1 
+// 1 => true
+// console.log(booleanIsLogged)
+
+let booleanNotLoggedIn = 0 
+// 0 => false
+// console.log(booleanNotLoggedIn)
+
+// (empty string) "" => 0
+// "Amr-cmd" => 1
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***************************Operations***************//
+
+let str1 = "hello"
+let str2 = " world"
+
+console.log(str1+str2)
+
+console.log("1"+2) // output = 12
+console.log("1"+2+2) // output = 122
+console.log(1+2+"2") //output = 32 
+
+// javascript treats every thing after a string as a string as shown in the above example
+
