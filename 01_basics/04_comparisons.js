3e14184945fc2eb58d000eaba7844e4318be20c6
Arm-cmd <armanthakur819@gmail.com>
2026-04-15 17:04:50 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+console.log("2">1) // output => true
+console.log("1">1) // output => false
+
+console.log(null > 0) // output => false
+console.log(null == 0) // output => false
+console.log(null >= 0)// output => true
+
+// === strict check
+
+console.log("2" === 2) // output = false
+
+console.log(undefined == 0) // output => false
+console.log(undefined >= 0) // output => false
