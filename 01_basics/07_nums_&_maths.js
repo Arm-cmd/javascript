fa73a953557cc5341a9cebc3fecf30cebe2f8df0
Arm-cmd <armanthakur819@gmail.com>
2026-04-29 08:55:30 +0000

    nums

--- 01_basics/07_nums_&_maths.js
+++ 01_basics/07_nums_&_maths.js
@@ -1,6 +1,34 @@
 const score = 400 
 
 const balance = new Number(100)
-console.log(balance)
+// console.log(balance)
 
-console.log(balance.toString())
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(3))
+
+const otherNum = 123.4564543
+// console.log(otherNum.toPrecision(5))
+
+const hundereds = 1000000
+//console.log(hundereds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++ MATHS +++++++++++++++++++++ // 
+
+// console.log(Math)
+
+// console.log(Math.round(4.65))
+// console.log(Math.celi(5.3))
+// console.log(Maath.floor(4.9))
+// console.log(Math.min(4,3,2,5))
+// console.log(Math.max(4,3,6,5))
+
+console.log(Math.random()) // generates a random number between 0 & 1
+console.log((Math.random()*10) + 1)
+console.log((Math.floor(Math.random()*10))+1)
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
